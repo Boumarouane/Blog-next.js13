e6fec46d1bfb41b0d7239597f0662b2a2565080e
Boumarouane <i.boumarouane@gmail.com>
2023-04-17 19:54:38 +0200

    arrange the post explores

--- traveling-blog/app/explore/page.jsx
+++ traveling-blog/app/explore/page.jsx
@@ -11,29 +11,33 @@ export const metadata = {
 
 function PostCard(post) {
   return (
-    <div className="cursor-pointer">
-      <Link
-        href={post.url}
-        legacyBehavior
-      >
-        <div className="mb-8 mx-8 relative w-[400px] h-[400px] md:w-[480px] md:h-[520px] overflow-hidden">
-          <Image
-            fill
-            className="w-full object-cover  h-auto hover:scale-105 transition-all duration-500 ease-in-out"
-            src={post.thumbnail}
-            alt="test"
-          />
-          <div className="absolute bottom-0 left-0 flex flex-col justify-center items-center w-full h-32 bg-white backdrop-filter backdrop-blur-md bg-opacity-10">
-            <time dateTime={post.date} className="block text-xs font-semibold">
-              {format(parseISO(post.date), "LLLL d, yyyy")}
-            </time>
-            <h3 className="text-2xl my-2 font-semibold text-slate-400">{post.title}</h3>
-            <div className="text-lg font-semibold text-slate-400">
-              {post.country}
+    <div className="lg:mt-28">
+      <div className="cursor-pointer">
+        <Link href={post.url} legacyBehavior>
+          <div className="mb-10 relative w-[350px] h-[350px] md:w-[480px] md:h-[520px] overflow-hidden">
+            <Image
+              fill
+              className="w-full object-cover h-auto hover:scale-105 transition-all duration-500 ease-in-out"
+              src={post.thumbnail}
+              alt="test"
+            />
+            <div className="absolute bottom-0 left-0 flex flex-col justify-center items-center w-full h-32 bg-white backdrop-filter backdrop-blur-md bg-opacity-10">
+              <time
+                dateTime={post.date}
+                className="block text-xs font-semibold"
+              >
+                {format(parseISO(post.date), "LLLL d, yyyy")}
+              </time>
+              <h3 className="text-2xl my-2 font-semibold text-slate-400">
+                {post.title}
+              </h3>
+              <div className="text-lg font-semibold text-slate-400">
+                {post.country}
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
   );
 }
@@ -46,8 +50,11 @@ export default function Explore() {
   );
 
   return (
-    <div>
-      <div className="py-8 mx-auto max-w-xl">
+    <div className="">
+      <div className="mx-auto max-w-x grid grid-cols-1 lg:grid-cols-2 justify-items-center">
+        <div className="h-auto lg:h-[500px] ml-0 md:ml-10 xl:ml-44 flex items-center justify-center py-10">
+          <h2 className=" text-4xl lg:text-7xl xl:text-8xl text-black">MY FEATURED TOURS</h2>
+        </div>
 
         {posts.map((post, idx) => (
           <PostCard key={idx} {...post} />
